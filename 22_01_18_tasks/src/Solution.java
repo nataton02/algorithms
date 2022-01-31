a4ee7e2db18a397a18d731e4562bd8301fb0cd86
Nataton <nataton02@gmail.com>
2022-01-31 16:28:23 +0100

    Contains Product, encode, decode

--- 22_01_18_tasks/src/Solution.java
+++ 22_01_18_tasks/src/Solution.java
@@ -1,4 +1,5 @@
 import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.Deque;
 
 public class Solution {
@@ -12,60 +13,78 @@ public class Solution {
      * @return
      */
     // Эффективно (О(n)) задача может быть решена с помощью ArrayDeque
-    public boolean containsProduct(int[] numbers, int product) {
-        Deque<Integer> arrayDeque = new ArrayDeque<>();
 
-        int first = numbers[0];
-        arrayDeque.addFirst(first);
+    public boolean containsProductEff(int[] numbers, int product) {
+        int i = 0;
+        int j = numbers.length - 1;
 
-        for (int i = numbers.length - 1; i > 0; i--) {
-            arrayDeque.addLast(numbers[i]);
-            if(first * arrayDeque.getLast() > product)
-                arrayDeque.removeLast();
-            if(first * arrayDeque.getLast() == product)
+        while(j - i > 0) {
+            int mul = numbers[i] * numbers[j];
+
+            if(mul < product)
+                i++;
+            else if(mul >product)
+                j --;
+            else
                 return true;
         }
         return false;
     }
 
 
-
-    // str - состоит только из маленьких букв.
+    //2.  str - состоит только из маленьких букв.
     // "aaaabccccd" -> "4a2b4cd"
     public String encode(String str) {
-        String res = "";
-        char current = str.charAt(0);
+        if (str.length() == 0)
+            return "";
+
         int count = 1;
+        StringBuilder builder = new StringBuilder();
 
-        for (int i = 1; i < str.length(); i++ ) {
-            if(str.charAt(i) == current) {
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i - 1) != str.charAt(i)) {
+                if (count > 1) {
+                    builder.append(count).append(str.charAt(i - 1));
+                    count = 1;
+                }
+                else {
+                    builder.append(str.charAt(i - 1));
+                }
+            } else
                 count++;
-            } else {
-                if (count == 1) {
-                    res += Character.toString(current);
-                } else {
-                    res += count + Character.toString(current);
-                }
-                current = str.charAt(i);
-                count = 1;
-            }
-
-            // last element
-            if (i == str.length() - 1) {
-                if (count == 1) {
-                    res += Character.toString(current);
-                } else {
-                    res += count + Character.toString(current);
-                }
-                return res;
-            }
         }
-        return  res;
+        if(count == 1)
+            builder.append(str.charAt(str.length() - 1));
+        else
+            builder.append(count).append(str.charAt(str.length() - 1));
+        return builder.toString();
     }
 
-    // str - состоит только из маленьких букв.
+    //3.  str - состоит только из маленьких букв.
     // "4a2b4cd" -> "aaaabbccccd"
     //public String decode(String str) {
 
-    //}
+    public String decode(String str) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            int digitsNum = 0;
+            while (Character.isDigit(str.charAt(i + digitsNum))) {
+                digitsNum++;
+            }
+
+            if(digitsNum > 0) {
+                int charNum = Integer.parseInt(str.substring(i, i + digitsNum));
+                i += digitsNum;
+
+                for (int j = 0; j < charNum; j++) {
+                    builder.append(str.charAt(i));
+                }
+            }
+            else {
+                builder.append(str.charAt(i));
+            }
+        }
+        return builder.toString();
+    }
 }
--- 22_01_18_tasks/src/SolutionTest.java
+++ 22_01_18_tasks/src/SolutionTest.java
@@ -6,37 +6,38 @@ class SolutionTest {
     Solution solution = new Solution();
 
     @Test
-    public void testContainsProduct_positive() {
-        int[] array = {2, 5, 18, 50};
+    public void testContainsProduct() {
+        int[] array = {2, 5, 18, 50, 100};
 
-        assertTrue(solution.containsProduct(array, 36));
-    }
+        assertTrue(solution.containsProductEff(array, 36));
+        assertTrue(solution.containsProductEff(array, 5000));
+        assertTrue(solution.containsProductEff(array, 250));
 
-    @Test
-    public void testContainsProduct_positive2() {
-        int[] array = {2, 5};
-
-        assertTrue(solution.containsProduct(array, 10));
-    }
-
-    @Test
-    public void testContainsProduct_negative() {
-        int[] array = {2, 5, 18, 50};
-
-        assertFalse(solution.containsProduct(array, 40));
+        assertFalse(solution.containsProductEff(array, 40));
+        assertFalse(solution.containsProductEff(array, 15000));
+        assertFalse(solution.containsProductEff(array, 1000000));
     }
 
     @Test
     public void testEncode_() {
-        String str = "aaaabbcccd";
-        assertEquals("4a2b3cd", solution.encode(str));
-        System.out.println(solution.encode(str));
+        assertEquals("4a2b4cd", solution.encode("aaaabbccccd"));
+        assertEquals("4ab4cd", solution.encode("aaaabccccd"));
+        assertEquals("4a2b4c2d", solution.encode("aaaabbccccdd"));
+        assertEquals("4a", solution.encode("aaaa"));
+        assertEquals("10a", solution.encode("aaaaaaaaaa"));
+        assertEquals("a", solution.encode("a"));
+        assertEquals("", solution.encode(""));
     }
 
     @Test
-    public void testEncode_2() {
-        String str = "abcdddeff";
-        assertEquals("abc3de2f", solution.encode(str));
-        System.out.println(solution.encode(str));
+    public void testSolutionDecode() {
+        assertEquals("aaaabbccccd", solution.decode("4a2b4cd"));
+        assertEquals("aaaabccccd", solution.decode("4ab4cd"));
+        assertEquals("aaaabbccccdd", solution.decode("4a2b4c2d"));
+        assertEquals("aaaa", solution.decode("4a"));
+        assertEquals("aaaaaaaaaa", solution.decode("10a"));
+        assertEquals("aaaaaaaaaaaaaaa", solution.decode("15a"));
+        assertEquals("a", solution.decode("a"));
+        assertEquals("", solution.decode(""));
     }
 }
